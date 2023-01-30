04c594610dda62d1821011250ab8fa74f3770890
Adrian Jurczuk <jurczuk.adrian@gmail.com>
2023-01-30 21:05:27 +0100

    przyciski informacje o paczce

--- script.js
+++ script.js
@@ -1,38 +1,67 @@
-function myFunction() {
-    const element = document.getElementById("id01");
-    element.innerHTML = "New Heading";
+function myFunction() {
+  const element = document.getElementById("id01");
+  element.innerHTML = "New Heading";
 }
 
-function myColorChange() {
-    const element = document.getElementById("id01");
-    element.style.color = "red";
+function myColorChange() {
+  const element = document.getElementById("id01");
+  element.style.color = "red";
 }
 
+const przesylka = {
+  name: "",
+  ulica: "",
+};
+
+const przesylka2 = {
+  name: "",
+  ulica: "",
+};
+
+przesylka2.waga = 20;
+przesylka.name = "List";
+przesylka2.name = "Paczka";
 
 
 
 
 
-function mojaFunkcja(imie) {
 
-    const clients = ["Mango", "Poly", "Ajax"];
-    const clientNameToFind = "Patryk";
-    let message;
+function mojaFunkcja(id) {
+
+  const element = document.getElementById("id01");
+  const element2 = document.getElementById("id02");
+
+  if ( id === 1){ //instrukcja dla przycisku z informacja o przesylce 1
+
+    informacja = "Typ twojej przesylki to: " + przesylka.name;
+    element.innerHTML = informacja;
+  
+  
+    informacja = "Przesyłka nie posiada wagi";
+    element2.innerHTML = informacja;
+
+  }
+
+  else if ( id === 2){ //instrukcja dla przycisku z informacja o przesylce 2
+
+    informacja = "Typ twojej przesylki to: " + przesylka2.name;
+    element.innerHTML = informacja;
+  
     
-    for (const client of clients) {
-    
-      if (client === imie) {
-        message = "Klient z takim imieniem jest w bazie danych!";
-        break;
-      }
-    
-      message = "Nie znaleźliśmy takiego klienta w bazie danych!";
-    }
-    
-    console.log(message);
-    
+    informacja = "Waga twojej przesylki to: " + przesylka2.waga + " kg";
+    element2.innerHTML = informacja;
+
+  }
 
 
+
+
+
+
+
+
+  
   
 
 }
