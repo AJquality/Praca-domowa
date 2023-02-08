b7ba274da8cda5dbd857f6dd67e09f3f2a102f46
Adrian Jurczuk <jurczuk.adrian@gmail.com>
2023-02-08 21:00:21 +0100

    zajecia 7

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function myFunction() {
 
 function myColorChange() {
   const element = document.getElementById("id01");
-  element.style.color = "red";
+  element.style.background = "red";
 }
 
 class Przesylka {
-  constructor(name, waga){
+  constructor(name, waga) {
     this.name = name;
     this.waga = waga;
   }
@@ -18,28 +18,25 @@ class Przesylka {
 let przesylka = new Przesylka("List", "");
 let przesylka2 = new Przesylka("Paczka", 20);
 
-
 function mojaFunkcja(id) {
-
   const element = document.getElementById("id01");
   const element2 = document.getElementById("id02");
 
-  if (id === 1) { //instrukcje dla przycisku z informacja o przesylce 1
+  if (id === 1) {
+    //instrukcje dla przycisku z informacja o przesylce 1
 
     informacja = "Typ twojej przesylki to: " + przesylka.name;
     element.innerHTML = informacja;
-    
+
     informacja = "Przesyłka nie posiada wagi";
     element2.innerHTML = informacja;
-  }
-
-  else if (id === 2) { //instrukcje dla przycisku z informacja o przesylce 2
+  } else if (id === 2) {
+    //instrukcje dla przycisku z informacja o przesylce 2
 
     informacja = "Typ twojej przesylki to: " + przesylka2.name;
     element.innerHTML = informacja;
-  
+
     informacja = "Waga twojej przesylki to: " + przesylka2.waga + " kg";
     element2.innerHTML = informacja;
   }
-
 }
